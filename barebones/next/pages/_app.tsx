00ff1fff42cf2e059b0221fe3d98289caba0d576
maxrawlinger <max.rawlinger@googlemail.com>
2022-11-29 18:00:11 +0100

    feat: add preview plugin; update utils

--- barebones/next/pages/_app.tsx
+++ barebones/next/pages/_app.tsx
@@ -1,12 +1,22 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { NinetailedProvider } from "@ninetailed/experience.js-next";
+import { NinetailedPreviewPlugin } from "@ninetailed/experience.js-plugin-preview";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <NinetailedProvider
       clientId={process.env.NEXT_PUBLIC_NINETAILED_CLIENT_ID || ""}
       environment={process.env.NEXT_PUBLIC_NINETAILED_ENVIRONMENT || "main"}
+      plugins={[
+        NinetailedPreviewPlugin({
+          clientId:
+            process.env.NEXT_PUBLIC_NINETAILED_MANAGEMENT_CLIENT_ID ?? "",
+          secret: process.env.NEXT_PUBLIC_NINETAILED_MANAGEMENT_SECRET ?? "",
+          environment: process.env.NEXT_PUBLIC_NINETAILED_ENVIRONMENT ?? "main",
+          ui: { opener: { hide: false } },
+        }),
+      ]}
     >
       <Component {...pageProps} />
     </NinetailedProvider>
