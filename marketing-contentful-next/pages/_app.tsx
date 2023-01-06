741236ce5d113e99ac26de8bd39310d6976a38ae
Chuck Kosman <chuck@ninetailed.com>
2023-01-06 14:45:43 -0500

    Service worker set IP

--- marketing-contentful-next/pages/_app.tsx
+++ marketing-contentful-next/pages/_app.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import '@/styles/globals.css';
 import { AppProps as NextAppProps } from 'next/app';
 import {
@@ -21,6 +21,23 @@ interface CustomPageProps {
 }
 
 const B2BDemoApp = ({ Component, pageProps }: AppProps<CustomPageProps>) => {
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      window.addEventListener('load', function () {
+        navigator.serviceWorker.register('/sw.js').then(
+          function (registration) {
+            console.log(
+              'Service Worker registration successful with scope: ',
+              registration.scope
+            );
+          },
+          function (err) {
+            console.log('Service Worker registration failed: ', err);
+          }
+        );
+      });
+    }
+  }, []);
   return (
     <div className="app">
       <HubspotProvider>
---
+++ marketing-contentful-next/public/sw.js
@@ -0,0 +1,25 @@
+self.addEventListener('install', function (event) {
+  console.log('⚙ Install');
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(customHeaderRequestFetch(event));
+});
+
+function customHeaderRequestFetch(event) {
+  if (event.request.url.includes('experience.ninetailed.co/v2/organizations')) {
+    console.log(
+      '⚙ Intercepting Ninetailed event request, setting X-Real-IP header'
+    );
+    const headers = new Headers(event.request.headers);
+    headers.set('X-Real-IP', '19.0.0.0');
+
+    const newRequest = new Request(event.request, {
+      mode: 'cors',
+      credentials: 'omit',
+      headers: headers,
+    });
+    return fetch(newRequest);
+  }
+  return fetch(event.request);
+}
