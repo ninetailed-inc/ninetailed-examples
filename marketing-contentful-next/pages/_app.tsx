304e45b7d172768e00e83a60cbe091b879cd2bee
Chuck Kosman <chuck@ninetailed.com>
2023-02-09 12:18:24 -0500

    Break out GTM between _app and _document per example

--- marketing-contentful-next/pages/_app.tsx
+++ marketing-contentful-next/pages/_app.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import '@/styles/globals.css';
 import { AppProps as NextAppProps } from 'next/app';
+import Script from 'next/script';
 import {
   ExperienceConfiguration,
   NinetailedProvider,
@@ -42,6 +43,19 @@ const B2BDemoApp = ({ Component, pageProps }: AppProps<CustomPageProps>) => {
           environment={process.env.NEXT_PUBLIC_NINETAILED_ENVIRONMENT ?? 'main'}
           experiments={pageProps.ninetailed?.experiments || []}
         >
+          <Script
+            id="gtm-base"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${
+                process.env.NEXT_PUBLIC_GTM_ID || ''
+              }');`,
+            }}
+          />
           <Component {...pageProps} />
         </NinetailedProvider>
       </HubspotProvider>
