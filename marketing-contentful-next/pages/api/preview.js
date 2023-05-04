c95cf30dc72d812ad81c9dfe866eb0fb6ae9f567
Chuck Kosman <chuck@ninetailed.com>
2023-05-04 11:53:33 -0700

    Remove console.log

--- marketing-contentful-next/pages/api/preview.js
+++ marketing-contentful-next/pages/api/preview.js
@@ -29,7 +29,6 @@ export default async function preview(req, res) {
   // Redirect to the path from the fetched post
   // We don't redirect to req.query.slug as that might lead to open redirect vulnerabilities
   // res.writeHead(307, { Location: formattedSlug });
-  console.log(url);
   res.setHeader('Content-Type', 'text/html');
   res.write(
     `<!DOCTYPE html><html><head><meta http-equiv="Refresh" content="0; url=${url}" />
