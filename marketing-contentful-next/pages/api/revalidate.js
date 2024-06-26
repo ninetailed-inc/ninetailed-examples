7a8863bdd1bdd07f9bd8b770c919e40d89816dc4
Chuck Kosman <chuck@ninetailed.com>
2024-06-26 15:25:51 -0700

    Webhook secret; demo revalidate all

--- marketing-contentful-next/pages/api/revalidate.js
+++ marketing-contentful-next/pages/api/revalidate.js
@@ -3,12 +3,23 @@
 // Why? Because for demo purposes it prevents having to refresh a page for ISR to kick in
 export default async function handler(req, res) {
   // Check for secret to confirm this is a valid request
-  // if (req.query.secret !== process.env.MY_SECRET_TOKEN) {
-  //   return res.status(401).json({ message: 'Invalid token' })
-  // }
+  if (
+    req.headers['x-ninetailed-secret'] !== process.env.WEBHOOK_CLIENT_SECRET
+  ) {
+    return res.status(401).json({ message: 'Invalid token' });
+  }
 
   try {
-    await Promise.all([res.revalidate('/')]);
+    // This is the part you don't want to copy for use anywhere. Live demo purposes only!
+    // TODO: Make dynamic by fetching all page-like entries and mapping their paths
+    await Promise.all([
+      res.revalidate('/'),
+      res.revalidate('/contact'),
+      res.revalidate('/products/chair'),
+      res.revalidate('/products/desk'),
+      res.revalidate('/blog/ergonomic-workspace-tips'),
+      res.revalidate('/blog/sustainable-office-benefits'),
+    ]);
     return res.json({ revalidated: true });
   } catch (err) {
     // If there was an error, Next.js will continue
